4a55be878c6dbd33c119dc1efc77aacee81c638f
Earlwiz <ealwizy2000@gmail.com>
2026-03-05 19:15:02 +0100

    Update App .jsx

--- src/App .jsx
+++ src/App .jsx
@@ -13,6 +13,19 @@ const products = [
     price: "$950",
     image: "Sofa2.jpg",
   },
+];const products = [
+  {
+    id: 1,
+    name: "Luxury Modern Sofa",
+    price: "$1,200",
+    image: "Sofa1.jpg",
+  },
+  {
+    id: 2,
+    name: "Premium Orange Couch",
+    price: "$950",
+    image: "Sofa2.jpg",
+  },
 ];
 
 export default function App() {
