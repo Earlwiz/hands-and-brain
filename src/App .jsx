23ce9209af7bf1d2180a56f074894d9175e0aa56
Earlwiz <ealwizy2000@gmail.com>
2026-03-05 19:10:58 +0100

    Update App .jsx

--- src/App .jsx
+++ src/App .jsx
@@ -5,13 +5,13 @@ const products = [
     id: 1,
     name: "Luxury Modern Sofa",
     price: "$1,200",
-    image: "/Sofa1.jpg",
+    image: "Sofa1.jpg",
   },
   {
     id: 2,
     name: "Premium Orange Couch",
     price: "$950",
-    image: "/Sofa2.jpg",
+    image: "Sofa2.jpg",
   },
 ];
 
